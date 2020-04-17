0d03a7a5e0f117d893ae4b92918e70f2419ac683
Adam Holtzman <adam.holtzman@gmail.com>
2020-04-17 17:57:41 -0400

    add level 4 and missed words

--- src/components/flash-cards.js
+++ src/components/flash-cards.js
@@ -4,24 +4,42 @@ import { shuffleArray } from './shared'
 export default ({ data, back }) => {
   const [ display, setDisplay ] = useState(0)
   const [ correct, setCorrect ] = useState(0)
+  const [ missed, setMissed ] = useState([])
   const btnStyle = { padding: '1rem 3rem', margin: '1rem', backgroundColor: 'rebeccapurple', color: 'white', fontWeigth: '700', fontSize: '1.5rem', fonstSize: '1rem', border: 0, cursor: 'pointer'}
   shuffleArray(data)
 
   return (
     <div style={{minHeight: '80vh', display: 'flex', justifyContent: 'space-between', flexDirection: 'column'}}>
-      <h1 style={{textAlign: 'center', fontSize: '6rem', padding: '2rem', margin: 'auto'}}>{ data[display] || correct }</h1>
+      <h1 style={{textAlign: 'center', fontSize: '6rem', padding: '2rem', margin: 'auto'}}>{ data[display] || `score of ${correct}` }</h1>
       <div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button
-          style={btnStyle}
-          disabled={!data[display]}
-          onClick={() => {
+      <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+      {data[display] && (
+        <>
+          <button
+            style={btnStyle}
+            disabled={!data[display]}
+            onClick={() => {
+              setDisplay( display + 1 )
+              setCorrect( correct + 1 )
+            }}>got it</button>
+          <button style={btnStyle} onClick={() => {
+            setMissed( prevState => [ ...prevState, data[display] ] )
             setDisplay( display + 1 )
-            setCorrect( correct + 1 )
-          }}>got it</button>
-        <button style={btnStyle} onClick={() => setDisplay( display + 1 )}>skip</button>
+          }}>skip</button>
+        </>
+      )
+      }
+        {
+          !data[display] && (
+            <>
+              {missed.map((word, index) => {
+              return <h3 style={{margin: '1rem'}} key={index}>{word}</h3>
+            })}
+            </>
+          )
+        }
       </div>
-      <p onClick={back}>return to list</p>
+      <p style={{marginTop: '3rem'}} onClick={back}>return to list</p>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import Cards from '../components/flash-cards'
 
 import one from '../data/one'
 import two from '../data/two'
+import three from '../data/three'
+import four from '../data/four'
 
 export default () => {
   const [ level, setLevel ] = useState(0)
@@ -18,8 +20,12 @@ export default () => {
     {level === 0 && <>
     <div style={{...lvl, backgroundColor: 'red', color: 'white'}} onClick={() => setLevel(1)}>Level 1</div>
     <div style={{...lvl, backgroundColor: 'blue', color: 'white'}}onClick={() => setLevel(2)}>Level 2</div>
+    <div style={{...lvl, backgroundColor: 'green', color: 'white'}}onClick={() => setLevel(3)}>Level 3</div>
+    <div style={{...lvl, backgroundColor: 'darkorange', color: 'white'}}onClick={() => setLevel(4)}>Level 4</div>
     </>}
     {level === 1 && <Cards data={one} back={() => setLevel(0)} />}
     {level === 2 && <Cards data={two} back={() => setLevel(0)} />}
+    {level === 3 && <Cards data={three} back={() => setLevel(0)} />}
+    {level === 4 && <Cards data={four} back={() => setLevel(0)} />}
   </Layout>
 )}
